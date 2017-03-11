b5ff8a0e0d8f1abaceaf04000f78d7973b1d6b0c
lanzoc <lanzo@hawaii.edu>
2017-03-11 08:52:31 -1000

    1

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,25 @@
 $(function() {
 
-  // your code will go here
+  $.ajax({
+    url: 'https://www.codeschool.com/users/lanzocamp.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      addCourses(response.courses.completed);
+    }
+  });
+
+  function addCourses(courses){
+
+    var badges = $('#badges');
+
+    courses.forEach(function(course){
+
+      $('<div />', {
+        'class': 'course'
+      }).appendTo($badges); 
+
+    })
+
+  }
 
 });
