e64593991bf41e62962f9d317c2e44b16c4578e8
lanzoc <lanzo@hawaii.edu>
2017-03-11 09:02:33 -1000

    3
    3

--- src/assets/main.js
+++ src/assets/main.js
@@ -10,13 +10,30 @@ $(function() {
 
   function addCourses(courses){
 
-    var badges = $('#badges');
+    var $badges = $('#badges');
 
     courses.forEach(function(course){
 
-      $('<div />', {
+      console.log(course);
+
+    var $course =  $('<div />', {
         'class': 'course'
       }).appendTo($badges);
+
+      $('<h3 />', {
+        text: course.title
+      }).appendTo($course);
+
+      $('<img />', {
+        src: course.badge
+      }).appendTo($course);
+
+      $('<a />',{
+        'class': 'btn btn-primary',
+        target: '_blank',
+        href: course.url,
+        text: 'See Course'
+      }).appendTo($course);
 
     })
 
